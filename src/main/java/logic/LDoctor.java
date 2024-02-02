144b481ec00f7d262e7c4c0f5602e35fcde653c7
vstrom60xt <cerveramonzo@gmail.com>
2024-02-02 15:32:40 +0100

    saddafsaddas

--- src/main/java/logic/LDoctor.java
+++ src/main/java/logic/LDoctor.java
@@ -1,21 +1,94 @@
 package logic;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Filters;
+import model.Doctor;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.Scanner;
 
 public class LDoctor {
-    public void addDoctor(MongoCollection<Document> collection,int id ,int numeroColegiado, String nombre , String especialidad){
+    Scanner sc = new Scanner(System.in);
 
+    public void addMedico(MongoCollection<Document> collection)  {
+        int id;
+        int numeroColegiado;
+        String nombre;
+        String especialidad;
+        System.out.println("id");
+        id = sc.nextInt();
+        System.out.println("numeroColegiado");
+        numeroColegiado= sc.nextInt();
+        System.out.println("nombre");
+        nombre= sc.next();
+        System.out.println("especialidad");
+        especialidad = sc.next();
+
+        Doctor doctor = new Doctor(id,numeroColegiado,nombre,especialidad);
         Document doctorDocument = new Document();
-        doctorDocument.append("_idDoctor",id);
-        doctorDocument.append("numeroColegiado",numeroColegiado);
-        doctorDocument.append("nombre",nombre);
-        doctorDocument.append("especialidad",especialidad);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = null;
+        try {
+            json = objectMapper.writeValueAsString(doctor);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        doctorDocument.append("medico",json);
+//        doctorDocument.append("_idDoctor",id);
+//        doctorDocument.append("numeroColegiado",numeroColegiado);
+//        doctorDocument.append("nombre",nombre);
+//        doctorDocument.append("especialidad",especialidad);
         collection.insertOne(doctorDocument);
         System.out.println("doctor insertado");
+    }
+
+    public void deleterMedico(MongoCollection<Document> collection, int id) {
+
+        MongoCursor <Document> cursor = collection.find(Filters.eq("idDoctor",id)).iterator();
+        while (cursor.hasNext()){
+            Document document = cursor.next();
+            System.out.println(document.toJson());
+        }
+        cursor.close();
+    }
 
 
+
+    public void deleteMedico(MongoCollection<Document> collection, int id) {
+//        MongoCursor<Document> cursor = collection.find(Filters.eq("_idDoctor", id)).iterator();
+//        while (cursor.hasNext()) {
+//            Document document = cursor.next();
+//            System.out.println("Deleting document: " + document.toJson());
+//
+//            collection.deleteOne(Filters.eq("_idDoctor", document.getObjectId(id)));
+//        }
+//
+//        cursor.close();
+
+        Bson filter = Filters.eq("_idDoctor",id);
+        FindIterable<Document> elementosEncon = collection.find(filter);
+        elementosEncon.forEach(document -> System.out.println(document));
+        System.out.println(elementosEncon.toString());
 
 
     }
+
+
+    public void findMedico(MongoCollection<Document> collection, int id) {
+        MongoCursor<Document> cursor = collection.find(Filters.eq("_idDoctor", id)).iterator();
+        while (cursor.hasNext()) {
+            Document document = cursor.next();
+            System.out.println("Deleting document: " + document.toJson());
+        }
+
+        cursor.close();
+    }
+
+
 }
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,10 +6,7 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import logic.LDoctor;
-import model.Doctor;
 import org.bson.Document;
-
-import java.util.Scanner;
 
 /**
  * Hello world!
@@ -17,26 +14,23 @@ import java.util.Scanner;
  */
 public class App{
     public static void main( String[] args ){
-        int id;
-        int numeroColegiado;
-        String nombre;
-        String especialidad;
 
-        Scanner sc = new Scanner(System.in);
-        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017");
-        MongoClient mongoClient = MongoClients.create(connectionString);
+        try {
+            ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017");
+            MongoClient mongoClient = MongoClients.create(connectionString);
+            MongoDatabase database = mongoClient.getDatabase("verehospital");
+            MongoCollection<Document> collection = database.getCollection("medico");
+            LDoctor a = new LDoctor();
+//
+//            a.addMedico(collection);
+            a.deleteMedico(collection,1);
 
-        MongoDatabase database = mongoClient.getDatabase("verehospital");
-        //MongoCollection<Document> collection = database.getCollection("");
-        System.out.println("id");
-        id = sc.nextInt();
-        System.out.println("numeroColegiado");
-        numeroColegiado= sc.nextInt();
-        System.out.println("nombre");
-        nombre= sc.nextLine();
-        System.out.println("especialidad");
-        especialidad = sc.nextLine();
-        Doctor doctor = new Doctor(id,numeroColegiado,nombre,especialidad);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
 
 
 
